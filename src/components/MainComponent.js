3c96c83abd0e45868af992d898907162b47a654f
Vihaan Nama <vihaan.nama@outlook.com>
2020-07-20 01:10:24 +0530

    Assignment 2 - Added the About Us Component

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,6 +5,7 @@ import Contact from './ContactComponent';
 import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import About from './AboutComponent';
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
@@ -46,6 +47,7 @@ class Main extends Component {
             <Route path='/home' component={HomePage} />
             <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
             <Route path='/menu/:dishId' component={DishWithId} />
+            <Route path= '/aboutus' component = {() => <About leaders={this.state.leaders} />} />
             <Route exact path='/contactus' component={Contact}/>
             <Redirect to="/home" />
         </Switch>
